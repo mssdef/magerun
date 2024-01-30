5b72f8158f71701d09aacd60351f7c69ee3b1834
Andrew <dev@xpscommerce.com>
2024-01-30 12:43:06 +0100

    Create console-apply-css.js

---
+++ css/console-apply-css.js
@@ -0,0 +1,1 @@
+document.head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="https://www.springs.nl/static/version1706865023/frontend/Magedev/Domodule/nl_NL/Magedev_CategoryDescription/css/categorydescription.css">');
