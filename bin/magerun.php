6070d203f45a5072aa899a84ae088650e074a68a
Andrew <dev@xpscommerce.com>
2024-01-30 11:58:24 +0100

    Update magerun.php

--- bin/magerun.php
+++ bin/magerun.php
@@ -8,18 +8,19 @@ use Magento\Framework\App\State;
 
 require __DIR__ . '/../app/bootstrap.php';
 
-if (!isset($argv[1])) {
-    die('Please add the class of the cron job you want to execute enclosed IN DOUBLE QUOTES as a parameter.' . PHP_EOL);
-} else {
-    define('CRONJOBCLASS', $argv[1]);
-}
-
 $bootstrap = Bootstrap::create(BP, $_SERVER);
 $obj = $bootstrap->getObjectManager();
 
 // Set the area code
 $state = $obj->get(State::class);
 $state->setAreaCode('crontab'); // Replace 'frontend' with the desired area code
+
+
+if (!isset($argv[1])) {
+    die('Please add the class of the cron job you want to execute enclosed IN DOUBLE QUOTES as a parameter.' . PHP_EOL);
+} else {
+    define('CRONJOBCLASS', $argv[1]);
+}
 
 $cron = $obj->create(CRONJOBCLASS);
 var_dump(get_class($cron));
