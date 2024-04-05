07182172dc764e94462da5aa13b0dea6906611d8
Andrew <dev@xpscommerce.com>
2024-04-05 20:21:48 +0200

    Update remove-unused-media-files.sh

--- bin/remove-unused-media-files.sh
+++ bin/remove-unused-media-files.sh
@@ -1,3 +1,9 @@
 find pub/media/catalog/product/ -type f -name "*.jpg" -exec basename {} \; | while read -r filename; do     grep -q "$filename" tmp/*.sql && echo -n "." || (echo "$filename not found in tmp/db.sql"); done
 
 # find pub/media/catalog/product/ -type f -name "*.jpg" -exec basename {} \; | while read -r filename; do     grep -q "$filename" tmp/*.sql && echo -n "." || (echo "$filename not found in tmp/db.sql"; find pub/media/catalog/product/ -type f -name "$filename" -exec mv {} tmp/media/ \;); done
+
+# nohup bash -c '
+# find pub/media/catalog/product/ -type f -name "*.jpg" -exec basename {} \; | while read -r filename; do
+#    grep -q "$filename" tmp/*.sql && echo -n "." || (echo "$filename not found in tmp/db.sql"; find pub/media/catalog/product/ -type f -name "$filename" -exec mv {} tmp/media/ \;);
+#done
+#' > output.log 2>&1 &
