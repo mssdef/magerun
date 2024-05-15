2d6e2eb6ac00a190a8a2db9e705ed4343580db7c
Andrew <dev@xpscommerce.com>
2024-05-15 12:09:07 +0200

    Create order-recalc.php

---
+++ bin/order-recalc.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * CLI run: php bin/order-recalc.php
+ **/
+use Magento\Framework\App\Bootstrap;
+
+require __DIR__ . '/../app/bootstrap.php';
+
+$bootstrap = Bootstrap::create(BP, $_SERVER);
+$obj = $bootstrap->getObjectManager();
+
+// Set the area code
+$obj->get(\Magento\Framework\App\State::class)->setAreaCode('adminhtml');
+
+//$orderId = 87;
+$orderId = 52;
+$orderRepository = $obj->create(Magento\Sales\Api\OrderRepositoryInterface::class);
+
+/**
+ * @var $order Magento\Sales\Model\Order
+ */
+$order = $orderRepository->get($orderId);
+$order->setTotalsCollectedFlag(false);
+//$order->setTotalRefunded(59.89);
+$orderRepository->save($order);
+
+foreach ($order->getAllItems() as $item) {
+    var_dump('item_getQtyToShip', $item->getId(), $item->getQtyToShip(), $item->getQtyRefunded());
+    $item->setQtyRefunded(1);
+    $item->save();
+}
+
+var_dump($order->getTotalRefunded(), $order->canShip());
